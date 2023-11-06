b8e3d601bcd05ba576ec624f284129ac8d7caff2
yihtseu <tongyz2922@mails.jlu.edu.cn>
2023-11-06 10:34:46 +0800

    fix: update gradle dependency configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,13 +88,14 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
 
     val appcompatVersion = "1.6.1"
     implementation("androidx.appcompat:appcompat:$appcompatVersion")
@@ -146,11 +147,11 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("com.google.accompanist:accompanist-permissions:0.32.0")
 
-    "googleImplementation"(platform("com.google.firebase:firebase-bom:32.2.2"))
+    "googleImplementation"(platform("com.google.firebase:firebase-bom:32.5.0"))
     "googleImplementation"("com.google.firebase:firebase-analytics-ktx")
     "googleImplementation"("com.google.firebase:firebase-messaging-ktx")
 
-    val huaweiVersion = "6.12.0.300"
+    val huaweiVersion = "1.9.1.300"
     "huaweiImplementation"("com.huawei.hms:push:$huaweiVersion")
     "huaweiImplementation"("com.huawei.hms:hmscoreinstaller:$huaweiVersion")
     "huaweiImplementation"("com.huawei.hms:hianalytics:$huaweiVersion")
